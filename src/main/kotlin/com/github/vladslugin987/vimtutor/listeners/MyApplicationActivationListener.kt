ac5c55ceb65fb2f5caf57cbf91c3244eeeed37ca
GitHub Action <action@github.com>
2025-02-02 23:24:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/vladslugin987/vimtutor/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/vladslugin987/vimtutor/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.vladslugin987.vimtutor.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
